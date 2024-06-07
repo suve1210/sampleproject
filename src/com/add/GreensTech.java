71562ae6a3dde319f4fd0c30751d2e753ba6389a
suve1210 <suvetha1210@gmail.com>
2024-06-07 12:17:50 -0700

    my first project

---
+++ src/com/add/GreensTech.java
@@ -0,0 +1,16 @@
+package com.add;
+
+
+public class Company
+
+
+public static void main(String[] args) {
+		
+		GreensTech g = new GreensTech();
+		g.greensOmr();
+		g.greensAdayar();
+		g.greensTambaram();
+		
+	}
+
+
